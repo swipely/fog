7626c8633a4ecf241f703a7466a6c5910d808cd9
Tom Hulihan <hulihan.tom159@gmail.com>
2014-01-13 10:28:45 -0500

    Add DataPipeline.query_all_objects.
    This method is similar to DataPipeline.query_objects, but it
    automatically follows pagination.

--- lib/fog/aws/requests/data_pipeline/query_objects.rb
+++ lib/fog/aws/requests/data_pipeline/query_objects.rb
@@ -13,9 +13,19 @@ module Fog
         # * Options <~Hash> - (optional):
         #   * 'Limit'<~Integer> - Maximum number of objects to return, default 100.
         #   * 'Marker'<~String> - The starting point for the results to be returned.
+        #   * 'Query'<~Hash> - Query that defines the objects to be returned
+        #     * 'Selectors'<~Array<Hash>>:
+        #       * 'FieldName'<~String> -
+        #       * 'Operator'<~Hash>:
+        #         * 'Type'<~String>
+        #         * 'Values'<~Array<String>>
         # ==== Returns
         # * response<~Excon::Response>:
         #   * body<~Hash>:
+        #     * 'HasMoreResults'<~Boolean> - If True, there are more results that can be
+        #                                    obtained by a subsequent call to QueryObjects.
+        #     * 'Ids'<~Array<String>> - A list of identifiers that match the query selectors.
+        #     * 'Marker'<String> - The starting point for the results to be returned.
         def query_objects(id, sphere, options={})
           params = {
             'pipelineId' => id,
@@ -30,10 +40,48 @@ module Fog
           Fog::JSON.decode(response.body)
         end
 
+        # Queries a pipeline for the names of objects that match a specified set of conditions.
+        # Unlike the above method, this method will automatically follow the pagination Marker
+        # until there are no results remaining.
+        # http://docs.aws.amazon.com/datapipeline/latest/APIReference/API_QueryObjects.html
+        # ==== Parameters
+        # * pipelineId <~String> - The ID of the pipeline
+        # * sphere <~String> - Specifies whether the query applies to components or instances.
+        #                      Allowable values: COMPONENT, INSTANCE, ATTEMPT.
+        # * Options <~Hash> - (optional):
+        #   * 'Limit'<~Integer> - Maximum number of objects to return, default 100.
+        #   * 'Marker'<~String> - The starting point for the results to be returned.
+        #   * 'Query'<~Hash> - Query that defines the objects to be returned
+        #     * 'Selectors'<~Array<Hash>>:
+        #       * 'FieldName'<~String> -
+        #       * 'Operator'<~Hash>:
+        #         * 'Type'<~String>
+        #         * 'Values'<~Array<String>>
+        # ==== Returns
+        # * response<~Excon::Response>:
+        #   * 'Ids'<~Array<String>> - A list of identifiers that match the query selectors.
+        def query_all_objects(id, sphere, options={})
+          has_more_results = true
+          objects = []
+
+          while has_more_results
+            result = query_objects(id, sphere, options).body
+            objects += result['Ids']
+            has_more_results = result['HasMoreResults']
+            options.merge!('Marker' => result['Marker']) if has_more_results
+          end
+
+          objects
+        end
+
       end
 
       class Mock
-        def query_objects(id, objects)
+        def query_objects(id, objects, params={})
+          Fog::Mock.not_implemented
+        end
+
+        def query_all_objects(id, objects, params={})
           Fog::Mock.not_implemented
         end
       end
